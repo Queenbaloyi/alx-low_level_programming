83514f7a2a20f2b2062f7c19c6cf37483409606b
QueenBaloyi <Queenbaloyi16@gmail.com.com>
2023-04-11 14:26:57 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,3 +11,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array = malloc(size);
+
+	if (size == 0 || array == 0)
+		return (NULL);
+	while (size--)
+		array[size] = c;
+	return (array);
+}
