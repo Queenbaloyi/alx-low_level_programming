4eb56e83ba07425501012943e285861aae2a25e2
QueenBaloyi <Queenbaloyi16@gmail.com.com>
2023-04-12 15:52:04 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,7 +4,7 @@
  * argstostr - main entry
  * @ac: int input
  * @av: double pointer array
- * Return: 0
+ * Return: 0 (success)
  */
 char *argstostr(int ac, char **av)
 {
@@ -13,12 +13,14 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
-			l++
+			l++;
 	}
 	l += ac;
+
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
@@ -36,4 +38,3 @@ char *argstostr(int ac, char **av)
 	}
 	return (str);
 }
-
