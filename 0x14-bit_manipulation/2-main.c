4552fca0a1978872e40bdeac34a78857fa5e9825
QueenBaloyi <Queenbaloyi16@gmail.com.com>
2023-05-06 15:29:12 -0700

    update

---
+++ 0x14-bit_manipulation/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main -code check
+*
+* Return: Always 0(zero).
+*/
+int main(void)
+{
+int n;
+n = get_bit(1024, 10);
+printf("%d\n", n);
+n = get_bit(98, 1);
+printf("%d\n", n);
+n = get_bit(1024, 0);
+printf("%d\n", n);
+return (0);
+}
